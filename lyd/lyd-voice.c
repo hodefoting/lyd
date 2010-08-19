69a44f5cd90ae7e6ad1e175630210ca700b2a00f
Simon Budig <simon@budig.de>
2010-08-20 00:05:10 +0200

    fix the LYD_EVENSIN waveform to match OPLs 1/4 sin waves.

--- lyd/lyd-voice.c
+++ lyd/lyd-voice.c
@@ -240,7 +240,7 @@ static inline LydSample lyd_voice_compute (LydVoice  *voice)
     /* OPL2 oscillators */
     OP(LYD_ABSSIN) OUT = fabs (sin (PHASE * M_PI * 2));
     OP(LYD_POSSIN) LydSample res = sin (PHASE * M_PI * 2); OUT = res>0?res:0.0;
-    OP(LYD_EVENSIN) float angle = PHASE * M_PI * 2; LydSample res = sin (angle); OUT = res>0?sin(angle*2):0.0;
+    OP(LYD_EVENSIN) OUT = fmod (PHASE, 0.5) < 0.25 ? fabs (sin (A(2) * M_PI * 2)) : 0.0;
 
     OP_FUN(LYD_ADSR,   adsr)
     OP_FUN(LYD_REVERB, voice_reverb)
